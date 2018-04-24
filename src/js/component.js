39927b0d1659fde371f0ff3294d0d8618b5dc130
d-kharchenko <dimonkharchenko89@gmail.com>
2018-04-24 10:19:03 +0300

    add table style

--- src/js/component.js
+++ src/js/component.js
@@ -8,4 +8,21 @@ $(function () {
     $('.to-top').bind('click',function(){
         $(this).fadeOut(200);  
     });
+    function adaptiveMenu(){
+    	if ($(window).width() < 768) {
+		    $('.header-navbar').attr('uk-offcanvas', '');
+		    $('.uk-navbar-container').attr('class', 'uk-offcanvas-bar uk-box-shadow-xlarge');
+		  }
+		 else {
+		 	$('#header-menu').removeAttr('uk-offcanvas');
+		 	$('#header-menu').attr('class','header-navbar')
+		    $('.header-navbar nav').attr('class','uk-navbar-container uk-navbar');
+
+		 }
+    }
+   
+	adaptiveMenu()
+	$(window).resize(function(){
+		adaptiveMenu()
+	})  
 })
